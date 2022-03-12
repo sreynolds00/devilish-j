59d169a8eb3e26b515c992b52043b6b45e1b491f
sreynolds00 <sammbob2006@gmail.com>
2022-03-12 22:18:47 +0000

    Discography section, minor changes
    added discography using javascript to build the gallery (mainly as practise as not necessary) and made some cool scrollbars

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// INSERTS IMAGES INTO DISCOGRAPHY
+
+const discography__grid = document.getElementById("discography__grid");
+const gallery_items = ["album-1.png","album-2.jpg","album-3.jpg","album-4.jpg","Kamikaze.jpg","2-pac.jpg","The_Eminem_Show.jpg"];
+
+for (let i = 0; i < gallery_items.length; i++){
+    container = document.createElement("div")
+    container.classList.add("container")
+    discography__grid.append(container)
+    image = document.createElement("img")
+    image.src = "assets/" + gallery_items[i];
+    image.classList.add("discography__image");
+    container.append(image);
+    imageCaption = document.createElement("div")
+    imageCaption.classList.add("discography__image-caption")
+    imageCaption.textContent = gallery_items[i].slice(0, -4)
+    container.append(imageCaption);
+}
